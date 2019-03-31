279cad993471c768f1b00ae30dc0e39246378dab
bhumika311 <bhumika0323@gmail.com>
2019-03-31 11:21:40 +0530

    Added HI path

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req,res) => {
     res.send('Hello! This is a sample app')
 })
 
+app.get('/hi', (req,res) =>{
+    res.send('Hello '+ req.query.name )
+})
+
 app.listen(PORT, () =>{
     console.log('server started')
 })
